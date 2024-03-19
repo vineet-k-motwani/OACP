a56a480abd73eccfeed2158b272b015c6ef24b02
vineet-motwani <motwanivineet08@gmail.com>
2024-03-19 17:15:20 +0530

    Task-3 March-19 Lab-2

---
+++ 3 Maximum Product Subarray/MaximumProductSubarray.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define MOD 1000000007
+#define vll vector<ll>
+
+signed main() {
+    int n;
+    cin>>n;
+    if (n==0) {
+        cout<<0;
+        return 0;
+    }
+    vector<int> nums(n);
+    for (int i=0; i<n; i++)
+        cin>>nums[i];
+    int ans = LLONG_MIN;
+    int dp=1;
+
+    for(int i=0; i<n; i++) {
+        dp *= nums[i];
+        ans = max(ans, dp);
+        if (nums[i]==0)
+            dp = 1;
+    }
+    dp=1;
+    for (int i=n-1; i>=0; i--) {
+        dp *= nums[i];
+        ans = max(ans, dp);
+        if (nums[i]==0)
+            dp=1;
+    }
+    cout<<ans;
+    return 0;
+}
